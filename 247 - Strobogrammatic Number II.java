4a620d9fd4df07049e7e6c08aac364f7f5ca84df
Cee <i@cee.moe>
2018-11-02 21:55:52 -0400

    11/2

---
+++ 247 - Strobogrammatic Number II.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> findStrobogrammatic(int n) {
+        return helper(n, n);
+    }
+
+    private List<String> helper(int n, int m) {
+        if (n == 0) { return new ArrayList<>(Arrays.asList("")); }
+        if (n == 1) { return new ArrayList<>(Arrays.asList("0", "1", "8")); }
+        
+        List<String> list = helper(n - 2, m);
+        List<String> res = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            if (n != m) { 
+                res.add("0" + s + "0"); 
+            }
+            res.add("1" + s + "1");
+            res.add("6" + s + "9");
+            res.add("8" + s + "8");
+            res.add("9" + s + "6");
+        }
+
+        return res;
+    }
+}
---
+++ 32 - Longest Valid Parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int left = 0, right = 0;
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                max = Math.max(max, 2 * right);
+            } else if (right >= left) {
+                left = right = 0;
+            }
+        }
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                max = Math.max(max, 2 * left);
+            } else if (left >= right) {
+                left = right = 0;
+            }
+        }
+        return max;
+    }
+}
